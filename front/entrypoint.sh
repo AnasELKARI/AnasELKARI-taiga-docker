d3e4696cc0bd7fb52441f5d803c8cbcc90390eb2
AnasELKARI <elkari.anas@gmail.com>
2016-04-11 16:01:44 +0100

    third

--- front/entrypoint.sh
+++ front/entrypoint.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
-sed -i "s/API_SERVER/$TAIGA_BACK_PORT_8000_TCP_ADDR/g" /usr/local/taiga/taiga-front/dist/js/conf.json
+sed -i "s/API_SERVER/192.168.1.111/g" /usr/local/taiga/taiga-front/dist/js/conf.json
 nginx -g "daemon off;"
