e508f3e86490f791dfb8eb936db729320fe0d277
AnasELKARI <elkari.anas@gmail.com>
2016-04-12 11:03:25 +0100

    test

--- front/entrypoint.sh
+++ front/entrypoint.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 mv conf.exemple.json conf.exemple112.json
-sed -i "s/API_SERVER/192.168.1.111/g" /usr/local/taiga/taiga-front/dist/conf.json
+sed -i "s/API_SERVER/$API_NAME/g" /usr/local/taiga/taiga-front/dist/conf.json
 nginx -g "daemon off;"
